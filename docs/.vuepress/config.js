b567bd54017ee2e068b29bedfed6fdb21e01501e
zx <1046529973@qq.com>
2021-12-18 22:25:44 +0800

    commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,10 +5,9 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  title: "Evan's blog",
+  title: "Xuan's blog",
   description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
-  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
-
+  base: '/godx-18/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   // 主题配置
   themeConfig: {
     // 导航配置
